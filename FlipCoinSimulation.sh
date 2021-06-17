ce631f59660be8acb2aefce4a6e272bbda21045f
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-17 19:32:58 +0530

    MutlipleFlips

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,8 +1,17 @@
 echo "Welcome to Flip Coin Simulation"
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+read -p "how many times you want to toss the coin: " num
+headcount=0
+tailcount=0
+for (( i=1; i<=$num; i++))
+do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		((headcount++))
+	else
+		((tailcount++))
+	fi
+done
+
+echo "Head won $headcount times"
+echo "Tail won $tailcount times"
